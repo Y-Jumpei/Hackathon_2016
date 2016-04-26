679260db056d71b432f656498c118c693dd7341e
haripo <ehutoad@gmail.com>
2016-04-26 12:21:03 +0900

    Modify note

---
+++ Assets/Script/NoteBehavior.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoteBehavior : MonoBehaviour
+{
+    public float Speed { get; set; }
+
+    private void Move(float distance)
+    {
+        transform.Translate(0, 0, -Speed);
+    }
+
+    void Start()
+    {
+        Speed = 0.5f;
+    }
+
+    void Update()
+    {
+        Move(Speed);
+    }
+}
