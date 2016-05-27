76cdee346c4c0bea03ed3258a478e42e4f6d5efd
haripo <ehutoad@gmail.com>
2016-05-27 18:10:14 +0900

    Change trigger of switching scene

--- Assets/Script/ResultController.cs
+++ Assets/Script/ResultController.cs
@@ -1,21 +1,31 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class ResultController : MonoBehaviour {
-	
-	public Text coolText;
-	public Text goodText;
-	public Text badText;
-	public Text maxcomboText;
+public class ResultController : MonoBehaviour
+{
+    public Text coolText;
+    public Text goodText;
+    public Text badText;
+    public Text maxcomboText;
 
-	void Start () 
-	{
-		maxcomboText.text = "Max Combo: " + ScoreController.MaxCombo;
-		coolText.text = "Cool: " + ScoreController.coolCount;
-		goodText.text = "Good: " + ScoreController.goodCount;
-		badText.text = "Bad: " + ScoreController.badCount;
+    private void OnSlideDetected(object sender, MotionDetectEventArgs e)
+    {
+        SceneManager.LoadScene("Title_CHURHYTHM");
+    }
 
-	}
-	
+    public void Start()
+    {
+        maxcomboText.text = "Max Combo: " + ScoreController.MaxCombo;
+        coolText.text = "Cool: " + ScoreController.coolCount;
+        goodText.text = "Good: " + ScoreController.goodCount;
+        badText.text = "Bad: " + ScoreController.badCount;
 
+        GetComponent<MotionDetector>().YSlide += OnSlideDetected;
+    }
+
+    public void OnDisable()
+    {
+        GetComponent<MotionDetector>().YSlide -= OnSlideDetected;
+    }
 }
--- Assets/Script/Title.cs
+++ Assets/Script/Title.cs
@@ -1,11 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Title : MonoBehaviour {
-	void FixedUpdate(){
-		if (Input.anyKeyDown) {
-			SceneManager.LoadScene("Game");
-		}
-	}
+public class Title : MonoBehaviour
+{
+    public void Start()
+    {
+        GetComponent<MotionDetector>().YSlide += (sender, e) =>
+        {
+            SceneManager.LoadScene("Game");
+        };
+    }
+
+    public void FixedUpdate()
+    {
+        if (Input.anyKeyDown)
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
 
 }
